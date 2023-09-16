7094385c5cf1f0475f271d05eac6b206200d92aa
Victor Hogemann <victor.hogemann@gmail.com>
2023-09-16 20:48:22 +0100

    Dockerfile

--- src/Test/Program.fs
+++ src/Test/Program.fs
@@ -1,4 +1,14 @@
 module Program =
-
+    open System.IO
     [<EntryPoint>]
-    let main _ = 0
+    let main _ =
+        let result =
+            File.ReadAllText "/var/rinha/source.rinha.json"
+            |> Rinha.Parser.parse
+        match result with
+        | Error msg ->
+            System.Console.Error.WriteLine msg
+            1
+        | Ok file ->
+            Rinha.Interpreter.Eval.evaluate Map.empty file.expression |> ignore
+            0
