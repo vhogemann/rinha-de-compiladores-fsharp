55d1f2a8b99e5de8d5558e3cb314e1712452deee
Victor Hogemann <victor.hogemann@gmail.com>
2023-09-16 23:51:56 +0100

    command line app

--- src/Rinha/Program.fs
+++ src/Rinha/Program.fs
@@ -1,2 +1,17 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System.IO
+
+let result =
+    File.ReadAllText "/var/rinha/source.rinha.json"
+    |> Rinha.Parser.parse
+match result with
+| Error msg ->
+    System.Console.Error.WriteLine msg
+    exit 1
+| Ok file ->
+    match Rinha.Interpreter.Eval.evaluate System.Console.Out Map.empty file.expression with
+    | Rinha.Interpreter.Value.Error err ->
+        System.Console.Error.WriteLine err
+        exit 1
+    | _ ->
+        exit 0
--- src/Test/Program.fs
+++ src/Test/Program.fs
@@ -1,18 +1,3 @@
 module Program =
-    open System.IO
     [<EntryPoint>]
-    let main _ =
-        let result =
-            File.ReadAllText "/var/rinha/source.rinha.json"
-            |> Rinha.Parser.parse
-        match result with
-        | Error msg ->
-            System.Console.Error.WriteLine msg
-            1
-        | Ok file ->
-            match Rinha.Interpreter.Eval.evaluate System.Console.Out Map.empty file.expression with
-            | Rinha.Interpreter.Value.Error err ->
-                System.Console.Error.WriteLine err
-                1
-            | _ ->
-                0
+    let main _ = 0
